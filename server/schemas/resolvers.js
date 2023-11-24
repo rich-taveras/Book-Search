ccc0b546367dfc5c359959f3f8ce88634303964e
rich-taveras <rtaveras@yahoo.com>
2023-11-24 06:50:22 -0500

    created schema and utils

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,85 @@
+const { User, Thought } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        
+//use ...book for short wait 
+        const user= await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { saveBooks: {...book} } }
+        );
+
+        return thought;
+      }
+      throw AuthenticationError;
+      ('You need to be logged in!');
+    },
+
+    removeBook: async (parent, { thoughtId }, context) => {
+      if (context.user) {
+        
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: {bookId} } }
+        );
+
+        return user;
+      }
+      throw AuthenticationError;
+    },
+    removeComment: async (parent, { thoughtId, commentId }, context) => {
+      if (context.user) {
+        return Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          {
+            $pull: {
+              comments: {
+                _id: commentId,
+                commentAuthor: context.user.username,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
