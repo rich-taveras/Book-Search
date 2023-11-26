04df3a6ab261db4347d74e447452a3ada50a102e
rich-taveras <rtaveras@yahoo.com>
2023-11-26 07:55:16 -0500

    recode client files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Book-Search
+
+Welcome to the Book Search App! This application allows users to search for books, view details about them, and manage their reading lists.
+
+## Features
+
+- **Search:** Easily search for books using keywords, titles, or authors.
+- **Details:** Get detailed information about each book, including title, author, publication date, and a brief description.
+- **Reading List:** Save your favorite books to a reading list for future reference.
+- **Responsive Design:** The app is designed to work seamlessly on various devices, including desktops, tablets, and smartphones.
+
+## Getting Started
+
+To run the Book Search App locally, follow these steps:
+
+Clone this repository:
+
+   git clone git@github.com:rich-taveras/Book-Search.git
+
+Dependencies
+React.js: A JavaScript library for building user interfaces.
+Axios: A promise-based HTTP client for making API requests.
+Your API Key: Obtain an API key from [your-book-api-provider] and replace it in the src/services/api.js file.
+
+## Contributing
+Richard Taveras
+
+## License
+This project is licensed under the MIT License.
+
+## Acknowledgments
+Thanks to Richard Taveras for the inspiration and guidance.
+Book data provided by Apollo.
+
+Happy reading!
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {
   Container,
   Card,
@@ -7,40 +7,25 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { QUERY_ME } from '../utils/queries';
+import { useQuery, useMutation } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+ 
+  const [RemoveBook]= useMutation(REMOVE_BOOK, [
+    
+    QUERY_ME,
+    'Me']);
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+const {loading, data}  =useQuery (QUERY_ME)
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+const savedBooks = data?.me.saveBooks|| []
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -51,14 +36,8 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const {data} = await RemoveBook({variables: {bookId}} );
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -67,7 +46,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
@@ -80,12 +59,12 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+          {savedBooks.length
+            ? `Viewing ${savedBooks.length} saved ${savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {savedBooks.map((book) => {
             return (
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
